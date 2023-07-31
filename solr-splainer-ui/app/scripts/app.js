d7c0c7067eed1eccead678718c4bed929fca14e4
Eric Pugh <epugh@opensourceconnections.com>
2023-07-31 18:39:48 -0400

    first attempt at a package!

---
+++ solr-splainer-ui/app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('splain-app', [
+    'ngSanitize',
+    'LocalStorageModule',
+    'ui.bootstrap',
+    'ngJsonExplorer',
+    'ui.ace',
+    'o19s.splainer-search'
+  ])
+  .config([
+    '$locationProvider',
+    function($locationProvider) {
+      $locationProvider.hashPrefix('');
+    }
+  ]);
+
+/* Override default config values for talking to Solr
+ * JSONP->GET
+ * */
+angular.module('o19s.splainer-search')
+  .value('defaultSolrConfig', {
+    sanitize:     true,
+    highlight:    true,
+    debug:        true,
+    numberOfRows: 10,
+    escapeQuery:  true,
+    apiMethod:    'GET'
+  });
